d81255dc677663b5e14300d1935d6c59fa38c86c
Binay <binayprogramming@gmail.com>
2021-01-31 20:52:31 +0100

    Made a dictionary to save the colors according to the number typed by user

--- Day-19-InstanceStateHOF/Project-Turtle-Race/main.py
+++ Day-19-InstanceStateHOF/Project-Turtle-Race/main.py
@@ -1,14 +1,14 @@
-from turtle import Screen,Turtle
-
+from turtle import Screen, Turtle
 
 screen = Screen()
 
 screen.setup(width=500, height=400, startx=-100, starty=-100)
 
 colors = ['red', 'green', 'orange', 'blue', 'yellow',  'purple']
+all_turtles = {}
 
-# user_choice_turtle = int(screen.numinput(title="Make your bet:", prompt='''Choose the color of your turtle:\n
-# Red = 1, Blue = 2, Purple = 3, Yellow = 4, Green = 5''', minval=1, maxval=5))
+user_choice_turtle = int(screen.numinput(title="Make your bet:", prompt='''Choose from number for your preferred color:
+1:Red, 2:Blue, 3:Orange, 4:Blue, 5:Yellow, 6:Purple''', minval=1, maxval=5))
 
 y_pos = -150
 for x in range(len(colors)):
@@ -16,7 +16,12 @@ for x in range(len(colors)):
     new_turtle.penup()
     new_turtle.color(colors[x])
     new_turtle.goto(-230, y_pos)
+    all_turtles[x+1] = colors[x]
     y_pos += 60
+
+#TODO = While turtle isn't on positon 220, don't stop the game
+
+
 
 
 screen.exitonclick()
