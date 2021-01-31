347adea0f96e2d655f5222976c458ce6c431c6fd
Binay <binayprogramming@gmail.com>
2021-01-31 21:19:49 +0100

    Project completed, turtle race == DONE

--- Day-19-InstanceStateHOF/Project-Turtle-Race/main.py
+++ Day-19-InstanceStateHOF/Project-Turtle-Race/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import random
 
 screen = Screen()
 
@@ -8,7 +9,7 @@ colors = ['red', 'green', 'orange', 'blue', 'yellow',  'purple']
 all_turtles = {}
 
 user_choice_turtle = int(screen.numinput(title="Make your bet:", prompt='''Choose from number for your preferred color:
-1:Red, 2:Blue, 3:Orange, 4:Blue, 5:Yellow, 6:Purple''', minval=1, maxval=5))
+1:Red, 2:Green, 3:Orange, 4:Blue, 5:Yellow, 6:Purple''', minval=1, maxval=6))
 
 y_pos = -150
 for x in range(len(colors)):
@@ -16,12 +17,22 @@ for x in range(len(colors)):
     new_turtle.penup()
     new_turtle.color(colors[x])
     new_turtle.goto(-230, y_pos)
-    all_turtles[x+1] = colors[x]
+    all_turtles[x+1] = new_turtle
     y_pos += 60
 
-#TODO = While turtle isn't on positon 220, don't stop the game
 
+game_running = True
+winner = None
+while game_running:
+    position = 220
+    for key in all_turtles:
+        all_turtles[key].forward(random.randint(0, 10))
+        if all_turtles[key].pos() > (220, all_turtles[key].pos()[1]):
+            game_running = False
+            winner = key
 
-
-
+if winner == user_choice_turtle:
+    print(f"Your {all_turtles[winner].color()[0]} turtle won the race!")
+else:
+    print(f"You lost. The {all_turtles[winner].color()[0]} turtle won the race!")
 screen.exitonclick()
