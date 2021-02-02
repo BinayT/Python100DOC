a6d2032f6101d6d2bcb0b9c0cf9ae2576ae1a022
Binay <binayprogramming@gmail.com>
2021-02-02 19:22:15 +0100

    Snake turning correctly

--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -9,25 +9,23 @@ screen.title('Snake Game')
 screen.tracer(0)
 
 snakes = []
-for x in range(10):
+for x in range(3):
     snakes.append(Snake(x_pos=0 - (x * 20), y_pos=0))
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
-    for x in range(len(snakes)):
-        snakes[x].turtle.forward(20)
-        if snakes[x].turtle.pos() >= (270, 0):
-            game_is_on = False
+    time.sleep(0.1)
 
-turn_left = True
-while turn_left:
+    for x in range(len(snakes)-1, 0, -1):
+        prev_x_cord = snakes[x-1].turtle.xcor()
+        prev_y_cord = snakes[x-1].turtle.ycor()
+        snakes[x].turtle.goto(prev_x_cord, prev_y_cord)
+    snakes[0].turtle.forward(20)
     snakes[0].turtle.left(90)
-    for x in range(1, len(snakes)-1):
-        snakes[x].turtle.setpos(snakes[x+1].turtle.pos())
-        snakes[0].turtle.forward(20)
-    turn_left = False
+
+
+
 
 
 
