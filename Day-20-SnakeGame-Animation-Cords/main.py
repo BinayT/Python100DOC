599365209423f835d0210a9920c344e08a0b912f
Binay <binayprogramming@gmail.com>
2021-02-02 19:00:36 +0100

    My try to turn the turtle

--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -9,17 +9,27 @@ screen.title('Snake Game')
 screen.tracer(0)
 
 snakes = []
-for x in range(3):
+for x in range(10):
     snakes.append(Snake(x_pos=0 - (x * 20), y_pos=0))
-
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.2)
-    for snake in snakes:
-        snake.turtle.forward(20)
-        if snakes[0].turtle.pos() >= (270, 0):
+    for x in range(len(snakes)):
+        snakes[x].turtle.forward(20)
+        if snakes[x].turtle.pos() >= (270, 0):
             game_is_on = False
 
+turn_left = True
+while turn_left:
+    snakes[0].turtle.left(90)
+    for x in range(1, len(snakes)-1):
+        snakes[x].turtle.setpos(snakes[x+1].turtle.pos())
+        snakes[0].turtle.forward(20)
+    turn_left = False
+
+
+
 screen.exitonclick()
+
