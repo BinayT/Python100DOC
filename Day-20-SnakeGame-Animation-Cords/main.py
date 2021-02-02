c38495c772a34f4d3d734c8a7cf676781030d411
Binay <binayprogramming@gmail.com>
2021-02-02 18:02:54 +0100

    Snake is now moving and entire piece is getting their animation updated

--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -1,20 +1,25 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import time
 from Snake_Game.snake_class import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+screen.tracer(0)
 
 snakes = []
 for x in range(3):
     snakes.append(Snake(x_pos=0 - (x * 20), y_pos=0))
 
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.2)
     for snake in snakes:
         snake.turtle.forward(20)
-        if snake.turtle.pos() >= (270, 0):
+        if snakes[0].turtle.pos() >= (270, 0):
             game_is_on = False
 
 screen.exitonclick()
