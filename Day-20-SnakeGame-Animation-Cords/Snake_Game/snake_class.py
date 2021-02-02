4fe68e109f04a9b3e596c133d6d0f96f6bbd1e92
Binay <binayprogramming@gmail.com>
2021-02-02 21:32:14 +0100

    Change in OOP approach

--- Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
+++ Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
@@ -1,9 +1,9 @@
 from turtle import Turtle
 
 
-class Snake:
-    def __init__(self, x_pos, y_pos):
-        self.turtle = Turtle('square')
-        self.turtle.penup()
-        self.turtle.color('white')
-        self.turtle.setpos(x_pos, y_pos)
+def snake(x_pos, y_pos):
+    tim = Turtle('square')
+    tim.penup()
+    tim.color('white')
+    tim.setpos(x_pos, y_pos)
+    return tim
--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -1,6 +1,6 @@
 from turtle import Screen
 import time
-from Snake_Game.snake_class import Snake
+from Snake_Game.snake_class import snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,8 +9,8 @@ screen.title('Snake Game')
 screen.tracer(0)
 
 snakes = []
-for x in range(3):
-    snakes.append(Snake(x_pos=0 - (x * 20), y_pos=0))
+for x in range(10):
+    snakes.append(snake(x_pos=0 - (x * 20), y_pos=0))
 
 game_is_on = True
 while game_is_on:
@@ -18,15 +18,11 @@ while game_is_on:
     time.sleep(0.1)
 
     for x in range(len(snakes)-1, 0, -1):
-        snakes[x].turtle.goto(snakes[x-1].turtle.pos())
+        snakes[x].goto(snakes[x-1].pos())
 
-    snakes[0].turtle.forward(20)
-    if snakes[0].turtle.pos() >= (270, 0) and snakes[0].turtle.heading() == 0:
-        snakes[0].turtle.left(90)
-
-
-
-
+    snakes[0].forward(20)
+    if snakes[0].pos() >= (270, 0):
+        snakes[0].left(90)
 
 
 screen.exitonclick()
