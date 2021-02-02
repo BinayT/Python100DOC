02dbb98589f4c4432a01e1ae80170aceebe7638f
Binay <binayprogramming@gmail.com>
2021-02-02 23:14:49 +0100

    Making the initial snake and moving it to our definative snake class

--- Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
+++ Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
@@ -1,9 +1,24 @@
 from turtle import Turtle
 
 
-def snake(x_pos, y_pos):
-    tim = Turtle('square')
-    tim.penup()
-    tim.color('white')
-    tim.setpos(x_pos, y_pos)
-    return tim
+class Snake:
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+
+    def create_snake(self):
+        for x in range(4):
+            turtle = Turtle('square')
+            turtle.color('white')
+            turtle.penup()
+            turtle.goto(0 - (x * 20), 0)
+            self.snakes.append(turtle)
+
+    def move(self):
+        for x in range(len(self.snakes) - 1, 0, -1):
+            self.snakes[x].goto(self.snakes[x - 1].pos())
+
+        self.snakes[0].forward(20)
+        if self.snakes[0].pos() >= (270, 0):
+            self.snakes[0].left(90)
+
--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
-from Snake_Game.snake_class import snake
+
+from Snake_Game.snake_class import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,21 +9,23 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
 
-snakes = []
-for x in range(10):
-    snakes.append(snake(x_pos=0 - (x * 20), y_pos=0))
+snake = Snake()
+# snakes = []
+# for x in range(10):
+#     snakes.append(snake(x_pos=0 - (x * 20), y_pos=0))
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for x in range(len(snakes)-1, 0, -1):
-        snakes[x].goto(snakes[x-1].pos())
-
-    snakes[0].forward(20)
-    if snakes[0].pos() >= (270, 0):
-        snakes[0].left(90)
+    snake.move()
+    # for x in range(len(snakes)-1, 0, -1):
+    #     snakes[x].goto(snakes[x-1].pos())
+    #
+    # snakes[0].forward(20)
+    # if snakes[0].pos() >= (270, 0):
+    #     snakes[0].left(90)
 
 
 screen.exitonclick()
