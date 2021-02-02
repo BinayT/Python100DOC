5ddf1528228210cb0fe38ca9b89c67c1829c8cf6
Binay <binayprogramming@gmail.com>
2021-02-02 23:38:19 +0100

    Refactored repetetive code

--- Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
+++ Day-20-SnakeGame-Animation-Cords/Snake_Game/snake_class.py
@@ -7,6 +7,7 @@ class Snake:
     def __init__(self):
         self.snakes = []
         self.create_snake()
+        self.head = self.snakes[0]
 
     def create_snake(self):
         for x in range(3):
@@ -21,21 +22,19 @@ class Snake:
             self.snakes[x].goto(self.snakes[x - 1].pos())
 
         self.snakes[0].forward(MOVE_DISTANCE)
-        # if self.snakes[0].pos() >= (270, 0):
-        #     self.snakes[0].left(90)
 
     def up(self):
-        if self.snakes[0].heading() == 0 or self.snakes[0].heading() == 180:
-            self.snakes[0].setheading(90)
+        if self.head.heading() == 0 or self.head.heading() == 180:
+            self.head.setheading(90)
 
     def down(self):
-        if self.snakes[0].heading() == 0 or self.snakes[0].heading() == 180:
-            self.snakes[0].setheading(270)
+        if self.head.heading() == 0 or self.head.heading() == 180:
+            self.head.setheading(270)
 
     def left(self):
-        if self.snakes[0].heading() == 90 or self.snakes[0].heading() == 270:
-            self.snakes[0].setheading(180)
+        if self.head.heading() == 90 or self.head.heading() == 270:
+            self.head.setheading(180)
 
     def right(self):
-        if self.snakes[0].heading() == 90 or self.snakes[0].heading() == 270:
-            self.snakes[0].setheading(0)
+        if self.head.heading() == 90 or self.head.heading() == 270:
+            self.head.setheading(0)
