9247b999490832ea0b10c861859c00b15cceffb3
Binay <binayprogramming@gmail.com>
2021-02-02 23:16:02 +0100

    main.py's refactored

--- Day-20-SnakeGame-Animation-Cords/main.py
+++ Day-20-SnakeGame-Animation-Cords/main.py
@@ -10,9 +10,6 @@ screen.title('Snake Game')
 screen.tracer(0)
 
 snake = Snake()
-# snakes = []
-# for x in range(10):
-#     snakes.append(snake(x_pos=0 - (x * 20), y_pos=0))
 
 game_is_on = True
 while game_is_on:
@@ -20,12 +17,6 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-    # for x in range(len(snakes)-1, 0, -1):
-    #     snakes[x].goto(snakes[x-1].pos())
-    #
-    # snakes[0].forward(20)
-    # if snakes[0].pos() >= (270, 0):
-    #     snakes[0].left(90)
 
 
 screen.exitonclick()
