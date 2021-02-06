fcfa86a23dec3d8b30b7fbfd605ac69009e9f57c
Binay <binayprogramming@gmail.com>
2021-02-06 01:16:07 +0100

    Both paddle's moving independently

--- Day-21-Pong/PongGame/main.py
+++ Day-21-Pong/PongGame/main.py
@@ -1,6 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
-from consts import SCREEN_BG_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH
+from consts import SCREEN_BG_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH, PADDLE_1, PADDLE_2
 
 screen = Screen()
 screen.bgcolor(SCREEN_BG_COLOR)
@@ -9,12 +9,18 @@ screen.title('Pong Game')
 screen.tracer(0)
 screen.listen()
 
-paddle = Paddle()
+paddle1 = Paddle(PADDLE_1)
+paddle2 = Paddle(PADDLE_2)
 
-screen.onkey(paddle.move_up, 'Up')
-screen.onkeypress(paddle.move_up, 'Up')
-screen.onkey(paddle.move_down, 'Down')
-screen.onkeypress(paddle.move_down, 'Down')
+screen.onkey(paddle1.move_up, 'Up')
+screen.onkeypress(paddle1.move_up, 'Up')
+screen.onkey(paddle1.move_down, 'Down')
+screen.onkeypress(paddle1.move_down, 'Down')
+
+screen.onkey(paddle2.move_up, 'w')
+screen.onkeypress(paddle2.move_up, 'w')
+screen.onkey(paddle2.move_down, 's')
+screen.onkeypress(paddle2.move_down, 's')
 
 game_is_on = True
 while game_is_on:
--- Day-21-Pong/PongGame/paddle.py
+++ Day-21-Pong/PongGame/paddle.py
@@ -2,30 +2,38 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, pos):
         super().__init__()
         self.paddle = []
-        self.create_paddle()
-        self.y_pos = self.paddle[0].pos()[1]
-        self.x_pos = self.paddle[0].pos()[0]
+        self.create_paddle(pos)
 
-    def create_paddle(self):
+    def create_paddle(self, pos):
         paddle = Turtle('square')
         paddle.color('white')
         paddle.penup()
         paddle.shapesize(stretch_len=1, stretch_wid=5)
-        paddle.goto(350, 0)
+        paddle.goto(pos)
         self.paddle.append(paddle)
 
     def move_up(self):
         y_pos = self.paddle[0].pos()[1]
+        x_pos = self.paddle[0].pos()[0]
         if y_pos >= 240:
             return
-        self.paddle[0].setpos(350, y_pos+20)
+
+        if x_pos == -360:
+            self.paddle[0].setpos(-360, y_pos+20)
+        else:
+            self.paddle[0].setpos(350, y_pos+20)
 
     def move_down(self):
         y_pos = self.paddle[0].pos()[1]
+        x_pos = self.paddle[0].pos()[0]
         if y_pos <= -240:
             return
-        self.paddle[0].setpos(350, y_pos-20)
+
+        if x_pos == -360:
+            self.paddle[0].setpos(-360, y_pos-20)
+        else:
+            self.paddle[0].setpos(350, y_pos-20)
 
