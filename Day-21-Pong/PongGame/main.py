eb5988d802e69f5f821df0ace3d604ba22372dd5
Binay <binayprogramming@gmail.com>
2021-02-06 01:05:35 +0100

    Turning off the animations and updating it with while loop

--- Day-21-Pong/PongGame/main.py
+++ Day-21-Pong/PongGame/main.py
@@ -6,6 +6,7 @@ screen = Screen()
 screen.bgcolor(SCREEN_BG_COLOR)
 screen.setup(height=SCREEN_HEIGHT, width=SCREEN_WIDTH)
 screen.title('Pong Game')
+screen.tracer(0)
 screen.listen()
 
 paddle = Paddle()
@@ -14,5 +15,9 @@ screen.onkey(paddle.move_up, 'Up')
 screen.onkeypress(paddle.move_up, 'Up')
 screen.onkey(paddle.move_down, 'Down')
 screen.onkeypress(paddle.move_down, 'Down')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
