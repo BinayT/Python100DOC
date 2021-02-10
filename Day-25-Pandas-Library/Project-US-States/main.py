bd193fd852ac74391348e3e771717b016da49b5d
Binay <binayprogramming@gmail.com>
2021-02-11 00:49:37 +0100

    Once we exit the app, it creates a new file that has the states we didn't include in the game

--- Day-25-Pandas-Library/Project-US-States/main.py
+++ Day-25-Pandas-Library/Project-US-States/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 from list_countries import data_obj
 
 screen = turtle.Screen()
@@ -12,12 +13,16 @@ while game_on:
     question = screen.textinput(f"{score}/50  States Correct", "Write a state's name:")
     if question == 'exit':
         game_on = False
+        file = pandas.DataFrame(data_obj)
+        file.to_csv("results.csv")
+
     answer = question.split(' ')
     definitive_ans = ''
     if len(answer) > 1:
         definitive_ans = f"{answer[0].capitalize()} {answer[1].capitalize()}"
     else:
         definitive_ans = question.capitalize()
+
     if definitive_ans in data_obj:
         x_axis = data_obj[definitive_ans][0]
         y_axis = data_obj[definitive_ans][1]
