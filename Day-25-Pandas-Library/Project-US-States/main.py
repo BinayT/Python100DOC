d53a85b527ca72fdec444de3adb376b93874eac6
Binay <binayprogramming@gmail.com>
2021-02-10 23:40:57 +0100

    Game done

--- Day-25-Pandas-Library/Project-US-States/main.py
+++ Day-25-Pandas-Library/Project-US-States/main.py
@@ -1,40 +1,29 @@
 import turtle
 from list_countries import data_obj
 
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = 'blank_states_img.gif'
+screen.bgpic(image)
 
+game_on = True
+score = 0
+while game_on:
+    question = screen.textinput(f"{score}/50  States Correct", "Write a state's name:")
+    answer = question.split(' ')
+    definitive_ans = ''
+    if len(answer) > 1:
+        definitive_ans = f"{answer[0].capitalize()} {answer[1].capitalize()}"
+    else:
+        definitive_ans = question.capitalize()
+    if definitive_ans in data_obj:
+        x_axis = data_obj[definitive_ans][0]
+        y_axis = data_obj[definitive_ans][1]
+        turtle.penup()
+        turtle.hideturtle()
+        turtle.goto(int(x_axis), int(y_axis))
+        turtle.write(definitive_ans)
+        data_obj.pop(definitive_ans)
+        score += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import turtle
-#
-# screen = turtle.Screen()
-# screen.title("U.S. State Game")
-# image = 'blank_states_img.gif'
-# screen.addshape(image)
-#
-# turtle.shape(image)
-#
-#
-# screen.exitonclick()
+turtle.mainloop()
