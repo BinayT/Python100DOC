1672f3f9e22734439f46fb409d4efb5d3dea703c
Binay <binayprogramming@gmail.com>
2021-02-12 00:17:10 +0100

    Dict comprehension using lists

--- Day-26-List-Comprehension/main.py
+++ Day-26-List-Comprehension/main.py
@@ -1,3 +1,4 @@
+from random import randint
 list_1_to_3 = [1, 2, 3]
 list_2_to_4 = [item+1 for item in list_1_to_3]
 
@@ -8,5 +9,6 @@ numbers_in_range = [num for num in range(1, 5)]
 numbers_in_range_doubled = [number for number in numbers_in_range if number % 2 == 0]
 
 names = ["Alex", "marTa", "JUaN", "BiNaY", "Pedro"]
-names_in_upper = [name.upper() for name in names if len(name)<5]
-print(names_in_upper)
+names_in_upper = [name.upper() for name in names if len(name) < 5]
+
+names_rand_score = {value : randint(0, 100) for value in names}
