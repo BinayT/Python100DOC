111521e0f2cdcb01d80aad7c95582d4f16a6df34
Binay <binayprogramming@gmail.com>
2021-02-11 13:03:49 +0100

    Conditional list comprehension using if

--- Day-26-List-Comprehension/main.py
+++ Day-26-List-Comprehension/main.py
@@ -1,3 +1,12 @@
 list_1_to_3 = [1, 2, 3]
 list_2_to_4 = [item+1 for item in list_1_to_3]
-print(list_2_to_4)
+
+name = "Binay"
+name_as_list = [char for char in name]
+
+numbers_in_range = [num for num in range(1, 5)]
+numbers_in_range_doubled = [number for number in numbers_in_range if number % 2 == 0]
+
+names = ["Alex", "marTa", "JUaN", "BiNaY", "Pedro"]
+names_in_upper = [name.upper() for name in names]
+print(names_in_upper)
