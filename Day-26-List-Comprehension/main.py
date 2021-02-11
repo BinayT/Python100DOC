f81bcf61b6d3858f311b726c5f8798f304efd1a6
Binay <binayprogramming@gmail.com>
2021-02-12 00:24:41 +0100

    Dict comprehension using already existing dictionary

--- Day-26-List-Comprehension/main.py
+++ Day-26-List-Comprehension/main.py
@@ -11,4 +11,8 @@ numbers_in_range_doubled = [number for number in numbers_in_range if number % 2 
 names = ["Alex", "marTa", "JUaN", "BiNaY", "Pedro"]
 names_in_upper = [name.upper() for name in names if len(name) < 5]
 
-names_rand_score = {value : randint(0, 100) for value in names}
+names_rand_score = {value: randint(0, 100) for value in names}
+
+names_with_score = {'Alex': 46, 'marTa': 91, 'JUaN': 12, 'BiNaY': 98, 'Pedro': 58}
+passed_students = {key: score for (key, score) in names_with_score.items() if score >= 60}
+print(passed_students)
