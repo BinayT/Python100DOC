94a229a6b72cc1a5bc47d874087d475746060152
Binay <binayprogramming@gmail.com>
2021-02-12 00:41:22 +0100

    Second exercise done with dict comprehension using already exisiting dict

--- Day-26-List-Comprehension/dict_challanges.py
+++ Day-26-List-Comprehension/dict_challanges.py
@@ -4,4 +4,20 @@
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 sentence_to_list = sentence.split(' ')
 length_count_sentence = {key: len(key) for key in sentence_to_list}
-print(length_count_sentence)
+
+# I am going to use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in
+# degrees Celsius and converts it into degrees Fahrenheit.
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {key: ((value * 9/5)+32) for (key, value) in weather_c.items()}
+
+print(weather_f)
