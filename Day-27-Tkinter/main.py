6d253ed4b59f087325d715129bd753d034513678
Binay <binayprogramming@gmail.com>
2021-02-14 02:13:17 +0100

    Buttons Entry and their settings learnt

--- Day-27-Tkinter/main.py
+++ Day-27-Tkinter/main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label
+from tkinter import *
 
 window = Tk()
 
@@ -6,6 +6,19 @@ window.title("My program")
 window.minsize(height=500, width=500)
 
 my_label = Label(text="I am a Label", font=("Courier", 24, 'bold'))
-my_label.pack(side="left")
+my_label.pack()
+
+
+inputt = Entry(width= 80)
+inputt.pack()
+
+
+def button_clicked():
+    print(inputt.get())
+
+
+button = Button(text="YO MAMA", command=button_clicked)
+button.pack()
+
 
 window.mainloop()
