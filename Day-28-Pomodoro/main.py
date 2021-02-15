1855871232ec43c9834bccd1f07779745a8f008f
Binay <binayprogramming@gmail.com>
2021-02-15 21:27:22 +0100

    Logic to make timing work created

--- Day-28-Pomodoro/main.py
+++ Day-28-Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,18 +9,37 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+window = Tk()
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+minute = 1
+sec = 0
 
+
+def yomama():
+    global minute, sec
+    window.after(1000, yomama)
+    if minute == 0 and sec == 0:
+        return
+    if sec == 0:
+        sec = 10
+        minute -= 1
+    sec -= 1
+
+    print(f'min:{minute} sec:{sec}')
+
+
+yomama()
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=60, bg=YELLOW)
+
+
 
 timer = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, '24', 'bold'))
 timer.grid(column=1, row=0)
@@ -36,7 +56,7 @@ reset.grid(row=2, column=2)
 canvas = Canvas(height=224, width=200, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+canvas.create_text(103, 130, text=f'{minute}:{sec}', fill="white", font=(FONT_NAME, 24, "bold"))
 canvas.grid(column=1, row=1)
 
 
