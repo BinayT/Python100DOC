38755c55862d54492c5e67bed5db9db211f33825
Binay <binayprogramming@gmail.com>
2021-02-15 20:02:53 +0100

    Basic layout done for our pomodoro GUI

--- Day-28-Pomodoro/main.py
+++ Day-28-Pomodoro/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=60, bg=YELLOW)
+
+timer = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, '24', 'bold'))
+timer.grid(column=1, row=0)
+
+start = Button(text="Start", highlightthickness=0)
+start.grid(row=2, column=0)
+
+label = Label(text="✅", fg=GREEN, bg=YELLOW)
+label.grid(row=3, column=1)
+
+reset = Button(text="Reset", highlightthickness=0)
+reset.grid(row=2, column=2)
+
+canvas = Canvas(height=224, width=200, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+canvas.grid(column=1, row=1)
+
+
+window.mainloop()
