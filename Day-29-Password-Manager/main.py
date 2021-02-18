02f5fb1458adb7025574447b7964fa98661a8f7e
Binay <binayprogramming@gmail.com>
2021-02-18 21:13:36 +0100

    Labels made and positioned correctly

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -4,9 +4,19 @@ window = Tk()
 window.title('Password Manager')
 window.config(padx=100, pady=50)
 
+# LOGO
 logo = Canvas(height=224, width=200, highlightthickness=0)
 logo_img = PhotoImage(file='logo.png')
 logo.create_image(110, 112, image=logo_img)
-logo.grid(column=1, row=1)
+logo.grid(column=1, row=0)
+
+# Labels and Entries
+website_label = Label(text="Website: ")
+website_label.grid(column=0, row=1)
+email_username_label = Label(text="Email/Username: ")
+email_username_label.grid(column=0, row=2)
+password_label = Label(text="Password: ")
+password_label.grid(column=0, row=3)
+
 
 window.mainloop()
