314a2f4d2f1c38840a2d7f681b992c28700d3e64
Binay <binayprogramming@gmail.com>
2021-02-18 21:41:43 +0100

    Connected the function to the generate password button and values are obtained perfectly

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -1,8 +1,18 @@
 from tkinter import *
+from password_gen import random_password_generator
 
+RANDOM_PASSWORD = None
 window = Tk()
 window.title('Password Manager')
 window.config(padx=100, pady=50)
+
+# Random password generator
+
+
+def create_password():
+    global RANDOM_PASSWORD
+    RANDOM_PASSWORD = random_password_generator()
+    print(RANDOM_PASSWORD)
 
 # LOGO
 logo = Canvas(height=224, width=200, highlightthickness=0)
@@ -25,7 +35,7 @@ password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3)
 password_entry = Entry()
 password_entry.grid(column=1, row=3)
-password_generate_button = Button(text="Generate Password")
+password_generate_button = Button(text="Generate Password", command=create_password)
 password_generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add")
