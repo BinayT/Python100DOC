943742c5b1e41fb65ae876585a6b753480c0a30a
Binay <binayprogramming@gmail.com>
2021-02-18 21:17:20 +0100

    Entry and generate password button added

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -13,10 +13,20 @@ logo.grid(column=1, row=0)
 # Labels and Entries
 website_label = Label(text="Website: ")
 website_label.grid(column=0, row=1)
+website_entry = Entry()
+website_entry.grid(column=1, row=1)
+
 email_username_label = Label(text="Email/Username: ")
 email_username_label.grid(column=0, row=2)
+email_entry = Entry()
+email_entry.grid(column=1, row=2)
+
 password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3)
+password_entry = Entry()
+password_entry.grid(column=1, row=3)
+password_generate_button = Button(text="Generate Password")
+password_generate_button.grid(column=2, row=3)
 
 
 window.mainloop()
