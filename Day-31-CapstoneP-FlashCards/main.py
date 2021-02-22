35d0543bec5a360a16d3c725e8f1a515d42d2758
Binay <binayprogramming@gmail.com>
2021-02-23 00:15:26 +0100

    We now getting random words

--- Day-31-CapstoneP-FlashCards/main.py
+++ Day-31-CapstoneP-FlashCards/main.py
@@ -2,25 +2,25 @@ from tkinter import *
 from tkinter import messagebox as mb
 from time import sleep
 import random
-import pandas
+import csv
 BACKGROUND_COLOR = "#B1DDC6"
 SCORE = 0
 
-# data = pandas.read_csv('data/french_words.csv', index_col=0, header=None, squeeze=True).to_dict()
-# print(data)
+with open("data/french_words.csv", "r") as f:
+    reader = csv.DictReader(f)
+    current_word = list(reader)
+
+random_word = random.choice(current_word)
 
 window = Tk()
 window.title('Flash Cards')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-label = Label(text="French", font=("Arial", 40, 'italic'), bg="white")
-label.place(x=400, y=150)
-
 logo = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 logo_img = PhotoImage(file='images/card_back.png')
 logo.create_image(400, 263, image=logo_img)
 logo.create_text(400, 150, text="French", font=("Arial", 40, 'italic'))
-logo.create_text(400, 263, text="trouve", font=("Arial", 40, 'italic'))
+logo.create_text(400, 263, text=random_word['French'], font=("Arial", 40, 'italic'))
 logo.grid(column=0, row=0, columnspan=2)
 
 wrong_image = PhotoImage(file='images/wrong.png')
