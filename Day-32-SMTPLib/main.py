cbeba4f6ca9c5e6dba17f4444764fb5b77fcfc1f
Binay <binayprogramming@gmail.com>
2021-02-24 13:25:43 +0100

    Date time module used to work with date and time

--- Day-32-SMTPLib/main.py
+++ Day-32-SMTPLib/main.py
@@ -1,13 +1,20 @@
-import smtplib
+# import smtplib
+# import datetime as dt
+#
+# my_email = 'mail@gmail.com'
+# password = 'thepassword'
+#
+# connection = smtplib.SMTP('smtp.gmail.com')
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(
+#     from_addr=my_email,
+#     to_addrs='thereciever@gmail.com',
+#     msg=f'Subject:Trying SMTP\n\nThis msg is sent using SMTP library of Python.\n{dt.datetime.now().date()}')
+# connection.close()
 
-my_email = 'mail@gmail.com'
-password = 'thepassword'
-
-connection = smtplib.SMTP('smtp.gmail.com')
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs='thereciever@gmail.com',
-    msg='Subject:Trying SMTP\n\nThis msg is sent using SMTP library of Python.')
-connection.close()
+import datetime as dt
+currentdatetime = dt.datetime.now()
+date = currentdatetime.date()
+time = currentdatetime.time()
+print(f'{date}, {time}')
