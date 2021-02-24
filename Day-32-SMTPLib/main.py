eb53b32f4bc3e15efc7d0b21514dc9b1c61b2788
Binay <binayprogramming@gmail.com>
2021-02-24 14:13:05 +0100

    Sending motivational quotes on every wednesday

--- Day-32-SMTPLib/main.py
+++ Day-32-SMTPLib/main.py
@@ -1,8 +1,9 @@
-# import smtplib
-# import datetime as dt
+import smtplib
+import datetime as dt
+from random import choice
 #
-# my_email = 'mail@gmail.com'
-# password = 'thepassword'
+my_email = 'sender@email.com'
+password = 'senderemailpassword'
 #
 # connection = smtplib.SMTP('smtp.gmail.com')
 # connection.starttls()
@@ -13,10 +14,27 @@
 #     msg=f'Subject:Trying SMTP\n\nThis msg is sent using SMTP library of Python.\n{dt.datetime.now().date()}')
 # connection.close()
 
-import datetime as dt
+# import datetime as dt
+# currentdatetime = dt.datetime.now()
+# date = currentdatetime.date()
+# time = currentdatetime.time()
+# randomdate = dt.datetime(year=1994, month=5, day=25, hour=19, minute=15)
+# print(randomdate)
+# print(f'{date}, {time}')
+
+with open('quotes.txt', mode="r") as quotes_file:
+    content = quotes_file.readlines()
+    content_list = []
+    for quote in content:
+        content_list.append(quote[:-1])
+
 currentdatetime = dt.datetime.now()
-date = currentdatetime.date()
-time = currentdatetime.time()
-randomdate = dt.datetime(year=1994, month=5, day=25, hour=19, minute=15)
-print(randomdate)
-print(f'{date}, {time}')
+day = currentdatetime.weekday()
+if day == 2:
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs='recieveremail@gmail.com',
+            msg=f"Subject:Motivational Wednesday\n\n{choice(content_list)}")
