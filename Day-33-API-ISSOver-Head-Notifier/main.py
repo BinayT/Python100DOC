3feaecf6b6c7e9e7b368e7c9c1bff4d560a2fa8e
Binay <binayprogramming@gmail.com>
2021-02-27 00:22:26 +0100

    Used an API to know sunset and sunrise of a specific position given lat and long

--- Day-33-API-ISSOver-Head-Notifier/main.py
+++ Day-33-API-ISSOver-Head-Notifier/main.py
@@ -6,5 +6,12 @@ latitude = reponse['iss_position']['latitude']
 longitude = reponse['iss_position']['longitude']
 
 iss_position = (latitude, longitude)
-print(iss_position)
+
+# https://api.sunrise-sunset.org/json
+
+sun_setrise_apicall = requests.get(f'https://api.sunrise-sunset.org/json?lat={iss_position[0]}'
+                                    f'&lng={iss_position[1]}')
+sun_setrise_apicall_res = sun_setrise_apicall.json()
+sun_setrise_apicall.raise_for_status()
+print(sun_setrise_apicall_res)
 
