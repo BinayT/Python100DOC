ba380c5bef44f0bf749c2219333794b8fb35a11c
Binay <binayprogramming@gmail.com>
2021-02-27 00:26:45 +0100

    Used another method to pass the params to the API

--- Day-33-API-ISSOver-Head-Notifier/main.py
+++ Day-33-API-ISSOver-Head-Notifier/main.py
@@ -1,17 +1,22 @@
 import requests
-
-reponse = requests.get('http://api.open-notify.org/iss-now.json').json()
-
-latitude = reponse['iss_position']['latitude']
-longitude = reponse['iss_position']['longitude']
-
-iss_position = (latitude, longitude)
+BARCELONA_LAT = 41.385063
+BARCELONA_LONG = 2.173404
+# reponse = requests.get('http://api.open-notify.org/iss-now.json').json()
+#
+# latitude = reponse['iss_position']['latitude']
+# longitude = reponse['iss_position']['longitude']
+#
+# iss_position = (latitude, longitude)
 
 # https://api.sunrise-sunset.org/json
 
-sun_setrise_apicall = requests.get(f'https://api.sunrise-sunset.org/json?lat={iss_position[0]}'
-                                    f'&lng={iss_position[1]}')
-sun_setrise_apicall_res = sun_setrise_apicall.json()
-sun_setrise_apicall.raise_for_status()
-print(sun_setrise_apicall_res)
+barcelona_sunset_sunrise_params = {
+    "lat": BARCELONA_LAT,
+    "lng": BARCELONA_LONG
+}
+
+sun_sunset_sunrise_apicall = requests.get(f'https://api.sunrise-sunset.org/json', params=barcelona_sunset_sunrise_params)
+sun_sunset_sunrise_apicall_res = sun_sunset_sunrise_apicall.json()
+sun_sunset_sunrise_apicall.raise_for_status()
+print(sun_sunset_sunrise_apicall_res)
 
