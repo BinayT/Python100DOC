8092cc69986c56796057eed0a154bcfacc800b23
Binay <binayprogramming@gmail.com>
2021-03-05 00:45:29 +0100

    Getting datas about the weather for next 12 hrs to see if it will rain or not

--- Day-35-Keys-Auth-Env/openweather.py
+++ Day-35-Keys-Auth-Env/openweather.py
@@ -2,13 +2,14 @@
 import requests
 from apikey import KEY
 
-user_city = input("Write down a city name...  ").lower()
+# user_city = input("Write down a city name...  ").lower()
 
-data_current_weather = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}'
-                                    f'&appid={KEY}&units=metric').json()
-data_onecall_api = requests.get(f'http://api.openweathermap.org/data/2.5/onecall?lat=41.449124&lon=41.449124'
-                                f'&exclude=hourly,daily&appid={KEY}&units=metric').json()
-print(data_onecall_api)
+# data_current_weather = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}'
+#                                     f'&appid={KEY}&units=metric').json()
+data_onecall_api = requests.get(f'http://api.openweathermap.org/data/2.5/onecall?lat=4.449124&lon=41.449124'
+                                f'&exclude=current,daily,alerts,minutely&appid={KEY}&units=metric').json()
+weather_12_hours = [x['weather'][0]['main'] for x in data_onecall_api['hourly'][:24]]
+print(weather_12_hours)
 # if data_current_weather["cod"] != 200:
 #     print(data_current_weather["message"])
 # else:
