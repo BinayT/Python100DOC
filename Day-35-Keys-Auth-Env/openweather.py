6246443c63b575b1229742daf0599dfd8181a714
Binay <binayprogramming@gmail.com>
2021-03-05 00:18:05 +0100

    Requested to data_onecall api to get all the data at once

--- Day-35-Keys-Auth-Env/openweather.py
+++ Day-35-Keys-Auth-Env/openweather.py
@@ -4,8 +4,12 @@ from apikey import KEY
 
 user_city = input("Write down a city name...  ").lower()
 
-data = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={KEY}&units=metric').json()
-if data["cod"] != 200:
-    print(data["message"])
-else:
-    print(f"Temp right now of {user_city} == {data['main']['temp']}ºC")
+data_current_weather = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}'
+                                    f'&appid={KEY}&units=metric').json()
+data_onecall_api = requests.get(f'http://api.openweathermap.org/data/2.5/onecall?lat=41.449124&lon=41.449124'
+                                f'&exclude=hourly,daily&appid={KEY}&units=metric').json()
+print(data_onecall_api)
+# if data_current_weather["cod"] != 200:
+#     print(data_current_weather["message"])
+# else:
+#     print(f"Temp right now of {user_city} == {data_current_weather['main']['temp']}ºC")
