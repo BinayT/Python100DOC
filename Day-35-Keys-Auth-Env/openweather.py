719f1f426c136491a103ef48c8913ebde68e1d6e
Binay <binayprogramming@gmail.com>
2021-03-05 00:00:19 +0100

    Better version to handle errors coming from the API

--- Day-35-Keys-Auth-Env/openweather.py
+++ Day-35-Keys-Auth-Env/openweather.py
@@ -5,7 +5,7 @@ from apikey import KEY
 user_city = input("Write down a city name...  ").lower()
 
 data = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={KEY}&units=metric').json()
-if data["cod"] == '404':
+if data["cod"] != 200:
     print(data["message"])
 else:
     print(f"Temp right now of {user_city} == {data['main']['temp']}ºC")
