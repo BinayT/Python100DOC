ed9b78485c0993d916f7444bcb9e0305a8a2f57a
Binay <binayprogramming@gmail.com>
2021-03-05 02:11:34 +0100

    Now we can send messages as SMS

--- Day-35-Keys-Auth-Env/openweather.py
+++ Day-35-Keys-Auth-Env/openweather.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import requests
-from apikey import KEY
+from apikey import KEY, TWILIO_SID, TWILIO_AUTH_TOKEN
+from twilio.rest import Client
 
 data_onecall_api_params = {
-    'lat': 4.449124,
-    'lon': 41.449124,
+    'lat': 39.215231,
+    'lon': -8.072670,
     'appid': KEY,
     "units": 'metric',
     "exclude": 'current,daily,alerts,minutely'
@@ -14,9 +15,20 @@ data_onecall_api_params = {
 
 # data_current_weather = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_city}'
 #                                     f'&appid={KEY}&units=metric').json()
+
 data_onecall_api = requests.get(f'http://api.openweathermap.org/data/2.5/onecall', data_onecall_api_params).json()
-weather_12_hours = [x['weather'][0]['main'] for x in data_onecall_api['hourly'][:24]]
-print(weather_12_hours)
+weather_12_hours = [x['weather'][0]['main'] for x in data_onecall_api['hourly'][:48] if x['weather'][0]['id'] < 900]
+
+
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+if len(weather_12_hours) > 0:
+    message = client.messages \
+        .create(
+        body="Trying Twilio.",
+        from_='MyNumber',
+        to='DestinationNumber'
+    )
+
 # if data_current_weather["cod"] != 200:
 #     print(data_current_weather["message"])
 # else:
