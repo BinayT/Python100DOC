e669cf0aae02f4cdeef62ca2ffb571c9aa7ff3a4
Binay <binayprogramming@gmail.com>
2021-03-09 00:42:02 +0100

    Posted new graph to the pixe.la and sent headers in the request

--- Day-37-Habit-Tracking-Project/main.py
+++ Day-37-Habit-Tracking-Project/main.py
@@ -1,11 +1,34 @@
 import requests
 
+USERNAME = 'binay'
+Token = "binaytoken"
+
+
 pixela_endpoint = 'https://pixe.la/v1/users'
 
 create_user_params = {
-    "token": "token",
+    "token": "binaytoken",
     "username": "binay",
     "agreeTermsOfService": "yes",
     "notMinor": 'yes'
 }
 
+# response = requests.post(url=pixela_endpoint, json=create_user_params)
+# print(response.text)
+
+graph_config = {
+    "id": "graph1",
+    "name": "My Graph",
+    "unit": "minute",
+    "type": "int",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": Token
+}
+
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+
+post_graph = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(post_graph.text)
