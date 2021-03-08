3ee4dde4bf64258a87c8886889a217b72cbfa086
Binay <binayprogramming@gmail.com>
2021-03-08 21:08:19 +0100

    Pixela params added

--- Day-37-Habit-Tracking-Project/main.py
+++ Day-37-Habit-Tracking-Project/main.py
@@ -1,3 +1,11 @@
 import requests
 
 pixela_endpoint = 'https://pixe.la/v1/users'
+
+create_user_params = {
+    "token": "token",
+    "username": "binay",
+    "agreeTermsOfService": "yes",
+    "notMinor": 'yes'
+}
+
