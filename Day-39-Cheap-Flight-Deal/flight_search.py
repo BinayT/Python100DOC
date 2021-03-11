20dcf68323d3a757a77a49b6dc88ed39e3cf2c85
Binay <binayprogramming@gmail.com>
2021-03-11 02:04:40 +0100

    Correctly making HTTP request to tequila API and getting IATA codes back for each cities

--- Day-39-Cheap-Flight-Deal/flight_search.py
+++ Day-39-Cheap-Flight-Deal/flight_search.py
@@ -1,9 +1,24 @@
+
 import requests
-APIKEY_TEQUILA = 'f55nrT6EMhIjueox-LYN1hO6iXc1Js7C'
-TEQUILA_ENDPOINT = 'https://tequila-api.kiwi.com/'
+headers = {
+    "apikey": '',
+}
+TEQUILA_ENDPOINT = 'https://tequila-api.kiwi.com/locations/query'
 
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
+    def __init__(self, cities_names):
+        self.cities_names = cities_names
+        print(self.cities_names)
 
-    pass
+    def post_IATA(self):
+        city_and_code = {}
+        for city in self.cities_names:
+            params = {
+                "term": city,
+                "locale": "en-US",
+                "location_types": "city"
+            }
+            get_info_city = requests.get(url=TEQUILA_ENDPOINT, headers=headers, params=params).json()
+            city_and_code[city] = get_info_city['locations'][0]['code']
--- Day-39-Cheap-Flight-Deal/main.py
+++ Day-39-Cheap-Flight-Deal/main.py
@@ -1,6 +1,14 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
+# -*- coding: utf-8 -*-
 from data_manager import DataManager
+from flight_search import FlightSearch
+
+cities = ['Paris', 'Berlin', 'Tokyo', 'Sydney', 'Istanbul', 'Kuala Lumpur', 'New York', 'San Francisco', 'Cape Town',
+          'Kathmandu']
+
 
 sheety_data_manager = DataManager()
-print(sheety_data_manager.get_data())
+
+flight_search = FlightSearch(cities)
+flight_search.post_IATA()
