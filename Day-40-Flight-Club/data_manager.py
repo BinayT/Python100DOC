afd0514be43e014e905ad158df7596d29e19fa44
Binay <binayprogramming@gmail.com>
2021-03-17 11:42:42 +0100

    Started day-40 with different approach

--- Day-40-Flight-Club/data_manager.py
+++ Day-40-Flight-Club/data_manager.py
@@ -1,29 +1,13 @@
-from pprint import pprint
 import requests
-
-SHEETY_PRICES_ENDPOINT = YOUR SHEETY PRICES ENDPOINT
+from keys_endpoints import SHEETY_ENDPOINT, SHEETY_AUTHORIZATION
 
 
 class DataManager:
-
+    # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.destination_data = {}
+        self.google_sheet_data = {}
 
-    def get_destination_data(self):
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
-        data = response.json()
-        self.destination_data = data["prices"]
-        return self.destination_data
+    def get_datas_from_sheety(self):
+        self.google_sheet_data = requests.get(url=SHEETY_ENDPOINT, headers=SHEETY_AUTHORIZATION).json()['prices']
 
-    def update_destination_codes(self):
-        for city in self.destination_data:
-            new_data = {
-                "price": {
-                    "iataCode": city["iataCode"]
-                }
-            }
-            response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
-                json=new_data
-            )
-            print(response.text)
+    # def update_iata_codes(self):
