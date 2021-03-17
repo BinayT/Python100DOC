05bf2de96540f5e4bdd5c16a611187f30d03b3a1
Binay <binayprogramming@gmail.com>
2021-03-17 13:44:37 +0100

    Structured flight data class correctly

--- Day-40-Flight-Club/flight_data.py
+++ Day-40-Flight-Club/flight_data.py
@@ -1,3 +1,10 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    def __init__(self, price, origin_city, destination_city, origin_airport, destination_airport, flight_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.destination_city = destination_city
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.flight_date = flight_date
+        self.return_date = return_date
