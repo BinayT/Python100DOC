a97622c4e131daa59b5f23c4ffe4fc90a8d0890b
Binay <binayprogramming@gmail.com>
2021-03-17 15:04:22 +0100

    Correctly displaying the first datas from tequila API

--- Day-40-Flight-Club/flight_search.py
+++ Day-40-Flight-Club/flight_search.py
@@ -1,15 +1,51 @@
 import requests
 from pprint import pprint
 from keys_endpoints import TEQUILA_ENDPOINT, TEQUILA_HEADERS
+from flight_data import FlightData
 
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
 
     def get_iata_codes(self, city_name):
-            params = {
-                "term": city_name,
-            }
-            tequila_locations_data = requests.get(url=f'{TEQUILA_ENDPOINT}/locations/query', headers=TEQUILA_HEADERS,
-                                                  params=params).json()
-            return tequila_locations_data['locations'][0]['code']
+        params = {
+            "term": city_name,
+        }
+        tequila_locations_data = requests.get(url=f'{TEQUILA_ENDPOINT}/locations/query', headers=TEQUILA_HEADERS,
+                                              params=params).json()
+        return tequila_locations_data['locations'][0]['code']
+
+    def check_flights(self, from_city, to_city, from_date, to_date):
+        params= {
+            'fly_from': from_city,
+            'fly_to': to_city,
+            'date_from': from_date.strftime("%d/%m/%Y"),
+            'date_to': to_date.strftime("%d/%m/%Y"),
+            'nights_in_dst_from': 7,
+            'nights_in_dst_to': 28,
+            'max_stopovers': 0
+        }
+
+        response = requests.get(url=f"{TEQUILA_ENDPOINT}/v2/search", headers=TEQUILA_HEADERS, params=params).json()
+
+        try:
+            data = response['data'][0]
+        except IndexError:
+            print(f'No flights for {from_city} -- {to_city}')
+            return None
+
+        flight_data = FlightData(
+            price=data['price'],
+            origin_city=data['route'][0]['cityFrom'],
+            destination_city=data['route'][0]['cityTo'],
+            origin_airport=data['route'][0]['flyFrom'],
+            destination_airport=data['route'][0]['flyTo'],
+            flight_date=data['route'][0]['local_departure'].split("T")[0],
+            return_date=data['route'][1]['local_departure'].split("T")[0],
+        )
+
+        print(f"{flight_data.origin_city} -> {flight_data.destination_city}, {flight_data.flight_date} ->"
+              f"{flight_data.return_date}, Price -> {flight_data.price}")
+        return flight_data
+
+
--- Day-40-Flight-Club/main.py
+++ Day-40-Flight-Club/main.py
@@ -1,5 +1,8 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime, timedelta
+
+ORIGIN_CITY = "BCN"
 
 sheety_data_manager = DataManager()
 sheety_data = sheety_data_manager.get_datas_from_sheety()
@@ -11,3 +14,14 @@ if sheety_data[0]['iataCode'] == '':
         iata_code = flight_search.get_iata_codes(city['city'])
         city['iataCode'] = iata_code
     sheety_data_manager.update_iata_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for city_data in sheety_data:
+    flight_search.check_flights(
+        from_city=ORIGIN_CITY,
+        to_city=city_data['iataCode'],
+        from_date=tomorrow,
+        to_date=six_month_from_today
+    )
