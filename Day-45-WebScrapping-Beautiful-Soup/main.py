bb5d5629358fa24b8445c735264dcd8a108cd2cd
Binay <binayprogramming@gmail.com>
2021-03-19 23:57:45 +0100

    Getting hold of different parts of our HTML soup

--- Day-45-WebScrapping-Beautiful-Soup/main.py
+++ Day-45-WebScrapping-Beautiful-Soup/main.py
@@ -4,4 +4,6 @@ with open('website.html', mode="r", encoding="utf-8") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
-    
+print(soup)
+print(soup.title)
+print(soup.title.string)
