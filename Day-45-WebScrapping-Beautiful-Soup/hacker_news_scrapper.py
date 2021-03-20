4a8ee41e3246ed9efd20d09e8f37319180be880f
Binay <binayprogramming@gmail.com>
2021-03-20 12:36:01 +0100

    Used list comprehension to get all the datas in a list at once

--- Day-45-WebScrapping-Beautiful-Soup/hacker_news_scrapper.py
+++ Day-45-WebScrapping-Beautiful-Soup/hacker_news_scrapper.py
@@ -6,11 +6,17 @@ URL = 'https://news.ycombinator.com/'
 ycombinator_data = requests.get(url=URL).text
 
 ycombinator_soup = BeautifulSoup(ycombinator_data, 'html.parser')
-storylink_class = ycombinator_soup.find(name="a", class_='storylink') # Getting a tag with classname of storylink
-storylink_class_text = storylink_class.getText() # Getting the text wrapped inside the a tag
-storylink_class_href_link = storylink_class.get("href") # Getting the value of href of the a tage
-print(storylink_class_href_link)
 
-article_upvote = ycombinator_soup.find(name='span', class_="score") # Getting a span tag with classname score
-article_upvote_number = article_upvote.getText().split(' ')[0] # Getting its text, splitting it and getting first el.
-print(article_upvote_number)
+# Getting a tag with classname of storylink
+storylink_class = ycombinator_soup.find_all(name="a", class_='storylink')
+# Getting the list of titles of the article wrapped inside the a tag.
+storylink_class_texts = [story.getText()for story in storylink_class]
+# Getting the list of links from the a tags.
+storylink_class_href_links = [story.get("href") for story in storylink_class]
+
+# Getting a span tag with classname score
+article_upvote = ycombinator_soup.find(name='span', class_="score")
+# Getting its text, splitting it and getting first element.
+article_upvote_number = int(article_upvote.getText().split()[0])
+
+
