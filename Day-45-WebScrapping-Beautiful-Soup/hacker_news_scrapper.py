8ba444019c19cf47efaa73c16bb30a401fb4506e
Binay <binayprogramming@gmail.com>
2021-03-20 01:33:38 +0100

    Scarping more and more in depth of the ycombinator website

--- Day-45-WebScrapping-Beautiful-Soup/hacker_news_scrapper.py
+++ Day-45-WebScrapping-Beautiful-Soup/hacker_news_scrapper.py
@@ -3,6 +3,15 @@ import requests
 
 URL = 'https://news.ycombinator.com/'
 
-ycombinator_data = requests.get(url=URL)
+ycombinator_data = requests.get(url=URL).text
 
-print(ycombinator_data.text)
+ycombinator_soup = BeautifulSoup(ycombinator_data, 'html.parser')
+storylink_class = ycombinator_soup.find(name="a", class_='storylink')
+storylink_class_text = storylink_class.getText()
+
+article_upvote = ycombinator_soup.find(name='span', class_="score")
+article_upvote_number = article_upvote.getText().split(' ')[0]
+print(article_upvote_number)
+
+article_link = ycombinator_soup.find(name="span", class_="sitestr").getText()
+print(article_link)
