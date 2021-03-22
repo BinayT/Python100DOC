318b32ecfc48285050579bac42eb909dec222ba9
Binay <binayprogramming@gmail.com>
2021-03-22 22:44:52 +0100

    Commented day 46's project

--- Day-46-Musical-Time-Machine-Spotify-POST/billboard_manager.py
+++ Day-46-Musical-Time-Machine-Spotify-POST/billboard_manager.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as BS
 import requests
+# Here I'm getting the URL for the site and date to scrape
 BILLBOARD_SITE = 'https://www.billboard.com/charts/hot-100/2010-03-06'
 
 
@@ -7,11 +8,17 @@ class BillboardManager:
     def scrape_top_100_songs(self):
         billboard_data = requests.get(url=BILLBOARD_SITE).text
 
+        # Parsing the text from the request to html, and with that we can work with BS
         billboard_soup = BS(billboard_data, 'html.parser')
+
+        # Getting the button that has class of button--link which contains the date
         hot100_of_day = billboard_soup.find(name="button", class_='button--link').getText()\
             .replace('\n', '').strip()
 
+        # Finding all spans with class name of color--primary which contains the name of the song.
         song_name = billboard_soup.find_all(name="span", class_="color--primary")
+        # Getting the text of the song and saving it in a list with list comprehension
         song_name_list = [song.getText() for song in song_name]
         data = [hot100_of_day, song_name_list]
+        # Sending songs and date as return
         return data
--- Day-46-Musical-Time-Machine-Spotify-POST/spotify_manager.py
+++ Day-46-Musical-Time-Machine-Spotify-POST/spotify_manager.py
@@ -3,19 +3,24 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 SPOTIFY_ENDPOINT = 'https://api.spotify.com/'
+# We can get all this from the spotify account and developer dashboard.
 client_id = 'CLIENT ID HERE'
 client_secret = 'CLIENT SECRET HERE'
 scope = 'playlist-modify-private'
 redirect_uri = 'http://example.com'
 username ="USER ID HERE"
+# cache_path === Name to be given to the .txt file
 cache_path = 'token'
 play_list_id = None
 
 
 class SpotifyManager:
     def __init__(self, top_100_songs_and_day):
+        # The top_100_songs_and_day is passed from billboard manager, which is a list with name and song list.
         self.top_100_songs = top_100_songs_and_day[1]
         self.day = top_100_songs_and_day[0]
+
+        # This is the way we can auth using spotipy package for spotify
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
             scope='playlist-modify-public', client_id=client_id,
             username=username, client_secret=client_secret, redirect_uri=redirect_uri, cache_path=cache_path))
@@ -29,12 +34,15 @@ class SpotifyManager:
 
         tracks_to_add = []
         for song in self.top_100_songs:
+            # Here we are trying to get track's uri, if there is no track, then it will simply pass to the next.
             try:
                 search_songs = self.sp.search(q=song, type='track')
                 track_uri = search_songs['tracks']['items'][0]['uri']
             except IndexError:
                 pass
             else:
+                # If there is no problem, then we append track's uri to the list.
                 tracks_to_add.append(track_uri)
 
+        # Then we finally add tracks in our playlist, items paramater is of type list.
         self.sp.playlist_add_items(playlist_id=play_list_id, items=tracks_to_add)
