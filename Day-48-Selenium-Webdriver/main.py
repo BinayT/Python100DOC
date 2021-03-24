d0b1a2c390354ae69c097f78f99d37f69a737a37
Binay <binayprogramming@gmail.com>
2021-03-24 18:42:04 +0100

    It's opening googleee

--- Day-48-Selenium-Webdriver/main.py
+++ Day-48-Selenium-Webdriver/main.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
 
-chromedriver_path = r'C:\Users\binay\OneDrive\Escritorio\Python\Chromedriver_win32'
+# Path of the chromedriver
+chromedriver_path = r'C:\Users\binay\OneDrive\Escritorio\Python\Chromedriver_win32\chromedriver.exe'
+
+# We choose chrome and the path will be where the chromedriver is located
 driver = webdriver.Chrome(executable_path=chromedriver_path)
+
+driver.get('https://www.google.com')
