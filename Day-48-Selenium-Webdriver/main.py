214045b498384d03b274731ea85e47d24d953667
Binay <binayprogramming@gmail.com>
2021-03-24 18:45:57 +0100

    Now we closing the tab as it opens and loads

--- Day-48-Selenium-Webdriver/main.py
+++ Day-48-Selenium-Webdriver/main.py
@@ -6,4 +6,6 @@ chromedriver_path = r'C:\Users\binay\OneDrive\Escritorio\Python\Chromedriver_win
 # We choose chrome and the path will be where the chromedriver is located
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 
+# Now we get the google's page and if we run this code then a chrome tab appears with google page.
 driver.get('https://www.google.com')
+driver.close()
