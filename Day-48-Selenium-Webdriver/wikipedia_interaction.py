01b53379c06db18c28cbb5273c6691b880c6eecf
Binay <binayprogramming@gmail.com>
2021-03-25 01:25:37 +0100

    Imported selenium common keys to enter 'Enter' button

--- Day-48-Selenium-Webdriver/wikipedia_interaction.py
+++ Day-48-Selenium-Webdriver/wikipedia_interaction.py
@@ -1,4 +1,5 @@
 from selenium_driver import driver
+from selenium.webdriver.common.keys import Keys
 
 wikipedia_mainpage_url = 'https://en.wikipedia.org/wiki/Main_Page'
 
@@ -9,6 +10,5 @@ articles_in_eng = driver.find_element_by_css_selector('#articlecount a:first-chi
 
 search_input = driver.find_element_by_name('search')
 search_input.send_keys('JavaScript')
-search = driver.find_element_by_name('go')
-search.click()
+search_input.send_keys(Keys.ENTER)
 # driver.close()
