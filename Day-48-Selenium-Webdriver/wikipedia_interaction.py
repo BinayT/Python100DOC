7995d3f894a7169687d084ce30fa595abfb37788
Binay <binayprogramming@gmail.com>
2021-03-25 01:23:27 +0100

    Selenium now writing JavaScript on the search field and searching it

--- Day-48-Selenium-Webdriver/wikipedia_interaction.py
+++ Day-48-Selenium-Webdriver/wikipedia_interaction.py
@@ -5,6 +5,10 @@ wikipedia_mainpage_url = 'https://en.wikipedia.org/wiki/Main_Page'
 driver.get(wikipedia_mainpage_url)
 
 articles_in_eng = driver.find_element_by_css_selector('#articlecount a:first-child')
-print(articles_in_eng.text)
+# articles_in_eng.click()
 
-driver.close()
+search_input = driver.find_element_by_name('search')
+search_input.send_keys('JavaScript')
+search = driver.find_element_by_name('go')
+search.click()
+# driver.close()
