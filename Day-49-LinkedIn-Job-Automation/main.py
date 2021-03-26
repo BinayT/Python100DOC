3144bee8965dc26992a454b89701fddca3a5c9ed
Binay <binayprogramming@gmail.com>
2021-03-27 00:54:08 +0100

    Now it checks if we can submit application at the first page or not, and if we can't, it will just close the tab

--- Day-49-LinkedIn-Job-Automation/main.py
+++ Day-49-LinkedIn-Job-Automation/main.py
@@ -1,13 +1,19 @@
 from selenium_driver import driver
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 url = 'https://www.linkedin.com'
-email = 'email@gmail.com'
-password = 'password'
+email = '@gmail.com'
+password = ''
 job_title = 'React Developer'
 job_location = 'Barcelona, Catalonia, Spain'
 
 # Gets inside LinkedIn
 driver.get(url)
+sleep(0.5)
+
+# Accepting cookies
+driver.find_element_by_xpath('//*[@id="artdeco-global-alert-container"]/div[1]/section/div/div[2]/button[2]').click()
+sleep(0.5)
 # Clicks login button
 click_login_button = driver.find_element_by_xpath('/html/body/nav/div/a[2]').click()
 
@@ -42,3 +48,17 @@ sleep(1)
 driver.find_element_by_xpath('//span[text()="Most recent"]').click()
 
 driver.find_element_by_xpath('//span[text()="Show results"]').click()
+sleep(2)
+job_cards = driver.find_element_by_class_name('job-card-container').click()
+
+apply_now = driver.find_element_by_xpath('//span[text()="Apply now"]').click()
+
+try:
+    submit_application = driver.find_element_by_xpath('//span[text()="Submit application"]')
+except NoSuchElementException:
+    driver.find_element_by_xpath('//button[@aria-label="Dismiss"]').click()
+    sleep(1)
+    driver.find_element_by_xpath('//span[text()="Discard"]').click()
+else:
+    driver.find_element_by_xpath('//label[contains(text()="Follow")]').click()
+    submit_application.click()
