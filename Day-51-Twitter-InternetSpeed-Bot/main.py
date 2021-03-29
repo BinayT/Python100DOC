76100016e5c1b512a5d2c9b2ede253e27968b51c
Binay <binayprogramming@gmail.com>
2021-03-30 01:45:58 +0200

    Enters twitter correctly

--- Day-51-Twitter-InternetSpeed-Bot/main.py
+++ Day-51-Twitter-InternetSpeed-Bot/main.py
@@ -5,11 +5,3 @@ from twitter import Twitter
 speedtest = SpeedTest(driver)
 speedtest.get_internet_data()
 twitter = Twitter(driver)
-
-
-# driver.maximize_window()
-# driver.get("https://www.cntraveller.in/")
-# # to fire up a new tab using javascript and load google.com
-# driver.execute_script('''window.open("https://www.google.com", "_blank");''')
-# time.sleep(5)
-# driver.quit()
--- Day-51-Twitter-InternetSpeed-Bot/twitter.py
+++ Day-51-Twitter-InternetSpeed-Bot/twitter.py
@@ -2,11 +2,22 @@ from data import email, password
 from time import time, sleep
 TWITTER_URL = 'https://www.twitter.com/'
 
+
 class Twitter:
     def __init__(self, driver):
         self.driver = driver
         self.create_new_tab()
+        self.enter_twitter()
 
     def create_new_tab(self):
         self.driver.execute_script('''window.open("https://www.twitter.com", "_blank");''')
+        self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(5)
+
+    def enter_twitter(self):
+        self.driver.find_element_by_xpath('//span[text()="Log in"]').click()
+        sleep(1)
+        email_field = self.driver.find_element_by_name('session[username_or_email]')
+        email_field.send_keys(email)
+        password_field = self.driver.find_element_by_name('session[password]')
+        password_field.send_keys(password)
