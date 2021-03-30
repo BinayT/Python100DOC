7b237a39efaf6d746eb5a86bf7f22913b1423ad3
Binay <binayprogramming@gmail.com>
2021-03-30 02:16:13 +0200

    Program now goes to the speedtest, gets ping and ups&downs, opens twitter and tweets it, project completed

--- Day-51-Twitter-InternetSpeed-Bot/main.py
+++ Day-51-Twitter-InternetSpeed-Bot/main.py
@@ -4,4 +4,12 @@ from twitter import Twitter
 
 speedtest = SpeedTest(driver)
 speedtest.get_internet_data()
+
+data_for_twitter = {
+    'ping': speedtest.ping,
+    'upload_speed': speedtest.upload_speed,
+    'download_speed': speedtest.download_speed
+}
+
 twitter = Twitter(driver)
+twitter.write_speed(data_for_twitter)
--- Day-51-Twitter-InternetSpeed-Bot/twitter.py
+++ Day-51-Twitter-InternetSpeed-Bot/twitter.py
@@ -1,6 +1,8 @@
 from data import email, password
-from time import time, sleep
+from time import sleep
+from datetime import datetime
 TWITTER_URL = 'https://www.twitter.com/'
+current_date_time = datetime.now().strftime("%B %d, %Y at %H:%M:%S")
 
 
 class Twitter:
@@ -14,10 +16,22 @@ class Twitter:
         self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(5)
 
-    def enter_twitter(self):
+    def click_login(self):
         self.driver.find_element_by_xpath('//span[text()="Log in"]').click()
+
+    def enter_twitter(self):
+        self.click_login()
         sleep(1)
         email_field = self.driver.find_element_by_name('session[username_or_email]')
         email_field.send_keys(email)
         password_field = self.driver.find_element_by_name('session[password]')
         password_field.send_keys(password)
+        self.click_login()
+        sleep(2)
+        self.driver.find_element_by_xpath('//span[text()="Close"]').click()
+
+    def write_speed(self, data):
+        write_internet_speed = self.driver.find_element_by_class_name('public-DraftStyleDefault-block')
+        write_internet_speed.send_keys(f"{current_date_time}\nThe internet's speed is\nPing:{data['ping']}\nDownload Speed"
+                                       f": {data['download_speed']}\nUpload Speed: {data['upload_speed']}")
+        self.driver.find_element_by_xpath('//span[text()="Tweet"]').click()
