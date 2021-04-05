16db5e23eaf403382f267f4ad6a66e70468c8c08
Binay <binayprogramming@gmail.com>
2021-04-06 00:09:17 +0200

    Advanced decorators to make a route's text bold

--- Day-55-HTML-URL-Parsing-Flask/main.py
+++ Day-55-HTML-URL-Parsing-Flask/main.py
@@ -2,15 +2,36 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(fun):
+    def make_boldd():
+        return f'<b>{fun()}</b>'
+    return make_boldd
+
+
+def make_emphasis(fun):
+    def make_emphasiss():
+        return f'<em>{fun()}</em>'
+    return make_emphasiss
+
+
+def make_underlined(fun):
+    def make_underlinedd():
+        return f'<u>{fun()}</u>'
+    return make_underlinedd
+
+
 @app.route('/')
+@make_bold
+@make_emphasis
+@make_underlined
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/<name>')
+@app.route('/<namee>')
 def say_hello(name):
-    return f'Hello, {name}!'
+    return f'Helloo, {name}!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
