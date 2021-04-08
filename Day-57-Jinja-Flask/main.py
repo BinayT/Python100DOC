7b229bd8727cc18ea6be86259ddb1c237b0a6164
Binay <binayprogramming@gmail.com>
2021-04-08 15:06:28 +0200

    Made a simple website that predicts gender and age of person and showing it in our flask app

--- Day-57-Jinja-Flask/main.py
+++ Day-57-Jinja-Flask/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import random
+import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +9,24 @@ app = Flask(__name__)
 @app.route('/')
 def say_hi():
     random_number = random.randint(0, 10)
-    return render_template('index.html', random_number=random_number)
+    current_year = datetime.datetime.now().year
+    data = {
+        'number': random_number,
+        'year': current_year
+    }
+    return render_template('index.html', data=data)
+
+
+@app.route('/guess/<name>')
+def guess_gender_age(name):
+    age = requests.get(f'https://api.agify.io?name={name}').json()
+    gender = requests.get(f'https://api.genderize.io?name={name}').json()
+    data = {
+        'age': age['age'],
+        'gender': gender['gender'],
+        'name': name
+    }
+    return render_template('genderize.html', data=data)
 
 
 if __name__ == '__main__':
