ff32d63eac37a12adf7a7146aabc4e4e8f851456
Binay <binayprogramming@gmail.com>
2021-04-16 23:52:28 +0200

    WTForms started, made custom validators and using it

--- Day-61-WTForms-Flask/main.py
+++ Day-61-WTForms-Flask/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisatopsecret?!?!'
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email : ', validators=[DataRequired()])
+    password = PasswordField('Password : ', validators=[DataRequired()])
 
 
 @app.route("/")
@@ -10,7 +19,8 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
