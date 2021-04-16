f46fcc042b5b714a55f5f42bed7a2cce41ac5886
Binay <binayprogramming@gmail.com>
2021-04-17 00:07:32 +0200

    Checking if email is a email and if password has minimum length of 8

--- Day-61-WTForms-Flask/main.py
+++ Day-61-WTForms-Flask/main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisatopsecret?!?!'
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email : ', validators=[DataRequired()])
-    password = PasswordField('Password : ', validators=[DataRequired()])
+    email = StringField('Email : ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password : ', validators=[DataRequired(), Length(min=8)])
     submit_label = SubmitField('Log In')
 
 
