8c4b537fdefe67d499005444ac93053d702e08f7
Binay <binayprogramming@gmail.com>
2021-04-17 10:14:14 +0200

    Project for day 61 completed

--- Day-61-WTForms-Flask/main.py
+++ Day-61-WTForms-Flask/main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisatopsecret?!?!'
+Bootstrap(app)
 
 
 class LoginForm(FlaskForm):
@@ -18,11 +20,31 @@ def home():
     return render_template('index.html')
 
 
+def render_success():
+    return render_template('success.html')
+
+
+def render_denied():
+    return render_template('denied.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    form.validate_on_submit()
+    if form.validate_on_submit():
+        email = 'admin@email.com'
+        password = '12345678'
+        if form.email.data == email and form.password.data == password:
+            return render_success()
+        else:
+            return render_denied()
     return render_template('login.html', form=form)
+
+
+@app.route('/form')
+def formm():
+    form = LoginForm()
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
