1e9850484181d715cb8678c3615a4f6ce9c0c40b
Binay <binayprogramming@gmail.com>
2021-04-21 19:37:00 +0200

    Created and added data to a database using SQLAlchemy

--- Day-63-Database-SQLite-SQLAlchemy/sqlite-practice/main.py
+++ Day-63-Database-SQLite-SQLAlchemy/sqlite-practice/main.py
@@ -1,7 +1,30 @@
-import sqlite3
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+#
+# cursor.execute("INSERT INTO books VALUES(2, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-cursor.execute("INSERT INTO books VALUES(2, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-sqlalchemy.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return '<Book %r>' % self.title
+
+
+book_detail = Book(id=5, title="Nepal Yatraaaa", author="Binay Timilsina", rating='9.6')
+db.session.add(book_detail)
+db.session.commit()
