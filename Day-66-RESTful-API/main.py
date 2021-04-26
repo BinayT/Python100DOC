9dc9a2ddf4dc936250e365bbf042c55cb7b1b204
Binay <binayprogramming@gmail.com>
2021-04-27 01:57:32 +0200

    Searching for a single cafe by their name completed

--- Day-66-RESTful-API/main.py
+++ Day-66-RESTful-API/main.py
@@ -10,11 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-def cafe_details(cafe):
-    cafe_to_dict = {column: str(getattr(cafe, column)) for column in cafe.__table__.c.keys()}
-    return cafe_to_dict
-
-
 # Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,28 +25,47 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+def cafe_details(cafe):
+    cafe_to_dict = {column: str(getattr(cafe, column)) for column in cafe.__table__.c.keys()}
+    return cafe_to_dict
+
+
+def cafes_list():
+    res = [cafe_details(cafe) for cafe in Cafe.query.all()]
+    return res
+
+
+all_cafes = cafes_list()
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
 # HTTP GET - Read Record
+
+# Reading a random record
 @app.route("/random", methods=['GET'])
 def random_cafe():
     cafe = random.choice(Cafe.query.all())
     return jsonify(cafe=cafe_details(cafe))
-    # return jsonify(cafe={
-    #     'name': cafe.name,
-    #     'can_take_calls': cafe.can_take_calls,
-    #     'coffee_price': cafe.coffee_price,
-    #     'has_sockets': cafe.has_sockets,
-    #     'has_toilet': cafe.has_toilet,
-    #     'has_wifi': cafe.has_wifi,
-    #     'img_url': cafe.img_url,
-    #     'location': cafe.location,
-    #     'map_url': cafe.map_url,
-    #     'seats': cafe.seats,
-    # })
+
+
+# Reading all record
+@app.route("/all", methods=['GET'])
+def get_all_cafes():
+    return jsonify(all_cafes=all_cafes)
+
+
+# Reading a record
+@app.route("/search", methods=['GET'])
+def get_a_cafe():
+    cafe_loc = request.args.get('loc')
+    for cafe in all_cafes:
+        if cafe['location'].lower() == cafe_loc.lower():
+            return jsonify(cafe=cafe)
+    return jsonify(error={'Not Found': f"Sorry, we couldn't find a cafe on {cafe_loc}"})
 
 # HTTP POST - Create Record
 
