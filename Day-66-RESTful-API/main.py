e48da6a12cf4fac8a273c3a7084b3dc73741b628
Binay <binayprogramming@gmail.com>
2021-04-27 01:18:20 +0200

    Sending random cafe as reponse

--- Day-66-RESTful-API/main.py
+++ Day-66-RESTful-API/main.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+def cafe_details(cafe):
+    cafe_to_dict = {column: str(getattr(cafe, column)) for column in cafe.__table__.c.keys()}
+    return cafe_to_dict
+
+
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -27,15 +33,31 @@ class Cafe(db.Model):
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
-## HTTP GET - Read Record
 
-## HTTP POST - Create Record
+# HTTP GET - Read Record
+@app.route("/random", methods=['GET'])
+def random_cafe():
+    cafe = random.choice(Cafe.query.all())
+    return jsonify(cafe=cafe_details(cafe))
+    # return jsonify(cafe={
+    #     'name': cafe.name,
+    #     'can_take_calls': cafe.can_take_calls,
+    #     'coffee_price': cafe.coffee_price,
+    #     'has_sockets': cafe.has_sockets,
+    #     'has_toilet': cafe.has_toilet,
+    #     'has_wifi': cafe.has_wifi,
+    #     'img_url': cafe.img_url,
+    #     'location': cafe.location,
+    #     'map_url': cafe.map_url,
+    #     'seats': cafe.seats,
+    # })
 
-## HTTP PUT/PATCH - Update Record
+# HTTP POST - Create Record
 
-## HTTP DELETE - Delete Record
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
